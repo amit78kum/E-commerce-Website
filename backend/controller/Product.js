f1f1ab4dabf3c57235d4a08506138667c20872ee
amit78kum <amkumar7488@gmail.com>
2024-05-18 18:10:19 +0530

    final

--- backend/controller/Product.js
+++ backend/controller/Product.js
@@ -3,6 +3,7 @@ const { Product } = require('../model/Product');
 exports.createProduct = async (req, res) => {
   // this product we have to get from API body
   const product = new Product(req.body);
+  product.discountPrice = Math.round(product.price*(1-product.discountPercentage/100))
   try {
     const doc = await product.save();
     res.status(201).json(doc);
@@ -25,14 +26,14 @@ exports.fetchAllProducts = async (req, res) => {
   let totalProductsQuery = Product.find(condition);
 
   if (req.query.category) {
-    query = query.find({ category: req.query.category });
+    query = query.find({ category: {$in:req.query.category.split(',')} });
     totalProductsQuery = totalProductsQuery.find({
-      category: req.query.category,
+      category: {$in:req.query.category.split(',')},
     });
   }
   if (req.query.brand) {
-    query = query.find({ brand: req.query.brand });
-    totalProductsQuery = totalProductsQuery.find({ brand: req.query.brand });
+    query = query.find({ brand: {$in:req.query.brand.split(',')} });
+    totalProductsQuery = totalProductsQuery.find({ brand: {$in:req.query.brand.split(',') }});
   }
   //TODO : How to get sort on discounted Price not on Actual price
   if (req.query._sort && req.query._order) {
@@ -72,7 +73,9 @@ exports.updateProduct = async (req, res) => {
   const { id } = req.params;
   try {
     const product = await Product.findByIdAndUpdate(id, req.body, {new:true});
-    res.status(200).json(product);
+    product.discountPrice = Math.round(product.price*(1-product.discountPercentage/100))
+    const updatedProduct = await product.save()
+    res.status(200).json(updatedProduct);
   } catch (err) {
     res.status(400).json(err);
   }
